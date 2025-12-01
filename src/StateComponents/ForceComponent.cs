6751e441753dd5173c883fa6ee5e48266296166e
Leonardo Raele <leonardoraele@gmail.com>
2025-12-01 17:17:39 -0300

    enh: Added tool script property filter to ForceComponent

--- src/StateComponents/ForceComponent.cs
+++ src/StateComponents/ForceComponent.cs
@@ -1,7 +1,9 @@
 using Godot;
+using Godot.Collections;
 
 namespace Raele.Supercon2D.StateComponents;
 
+[Tool]
 public partial class ForceComponent : SuperconStateController
 {
 	// -----------------------------------------------------------------------------------------------------------------
@@ -11,6 +13,7 @@ public partial class ForceComponent : SuperconStateController
 	public enum ForceTypeEnum
 	{
 		FixedDirection,
+		Gravity,
 		FacingDirection,
 		MovementResistance,
 	}
@@ -19,9 +22,29 @@ public partial class ForceComponent : SuperconStateController
 	// EXPORTS
 	// -----------------------------------------------------------------------------------------------------------------
 
-	[Export] public Vector2 Direction = Vector2.Down;
-	[Export] public float AccelerationPxPSecSqr = 600f;
+	[Export] public ForceTypeEnum ForceType
+		{ get => field; set { field = value; this.NotifyPropertyListChanged(); } } = ForceTypeEnum.FixedDirection;
+	[Export] public Vector2 Direction = Vector2.Zero;
+	[Export] public float AccelerationPxPSecSq = 600f;
 	[Export] public float MaxSpeedPxPSec = 600f;
+
+	// -----------------------------------------------------------------------------------------------------------------
+	// EXPORTS
+	// -----------------------------------------------------------------------------------------------------------------
+
+	public Vector2 ComputedDirection => this.ForceType switch
+	{
+		ForceTypeEnum.FixedDirection => this.Direction,
+		ForceTypeEnum.Gravity => ProjectSettings.GetSetting("physics/2d/default_gravity_vector").AsVector2(),
+		ForceTypeEnum.FacingDirection => Vector2.Right * this.Character.FacingDirection,
+		ForceTypeEnum.MovementResistance => this.Character.Velocity.Normalized() * -1,
+		_ => Vector2.Zero,
+	};
+	public float ComputedAccelerationPxPSecSq => this.ForceType switch
+	{
+		ForceTypeEnum.Gravity => ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle(),
+		_ => this.AccelerationPxPSecSq,
+	};
 
 	// -----------------------------------------------------------------------------------------------------------------
 	// LIFECYCLE METHODS
@@ -29,10 +52,36 @@ public partial class ForceComponent : SuperconStateController
 
 	public override void _PhysicsProcessActive(double delta)
 	{
+		if (Engine.IsEditorHint())
+		{
+			return;
+		}
 		base._PhysicsProcessActive(delta);
 		this.Character.Accelerate(
-			this.Direction.Normalized() * this.MaxSpeedPxPSec,
-			this.AccelerationPxPSecSqr * (float) delta
+			this.ComputedDirection.Normalized() * this.MaxSpeedPxPSec,
+			this.ComputedAccelerationPxPSecSq * (float) delta
 		);
+	}
+
+	public override void _ValidateProperty(Dictionary property)
+	{
+		base._ValidateProperty(property);
+		switch (property["name"].AsString())
+		{
+			case nameof(Direction):
+				property["usage"] = (long) (this.ForceType switch
+				{
+					ForceTypeEnum.FixedDirection => PropertyUsageFlags.Default,
+					_ => PropertyUsageFlags.NoEditor,
+				});
+				break;
+			case nameof(AccelerationPxPSecSq):
+				property["usage"] = (long) (this.ForceType switch
+				{
+					ForceTypeEnum.Gravity => PropertyUsageFlags.NoEditor,
+					_ => PropertyUsageFlags.Default,
+				});
+				break;
+		}
 	}
 }
